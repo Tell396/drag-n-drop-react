5a9f8f1db015f36d403bfc4bad3db7a4121d3290
Tell396 <vova21473@gmail.com>
2022-05-08 16:32:50 +1000

    Story #1. Main features, styles were added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-export default App;
+// Import an engine for Drag&Drop
+import Container from './engine/Container'
+
+const App = () => {
+    return (
+        <div className="flex justify-center align-center mt-10">
+            {/* Create a Drag&Drop zone: */}
+            <DndProvider backend={HTML5Backend}>
+                <div className="flex">
+                    <Container />
+                </div>
+
+                <hr />
+
+                <div className="flex">
+                    <Container />
+                </div>
+            </DndProvider>
+        </div>
+    );
+};
+
+export default App
