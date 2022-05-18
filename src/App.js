ef99c15adafd1beabfa469a87553b5437040527e
Tell396 <vova21473@gmail.com>
2022-05-18 18:00:35 +1000

    Story #3. Global changes.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,134 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { v4 as uuid } from 'uuid';
 
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+const itemsPackOne = [
+  { id: uuid(), content: 'First task' },
+  { id: uuid(), content: 'Second task' },
+  { id: uuid(), content: 'Third task' },
+  { id: uuid(), content: 'Fourth task' },
+  { id: uuid(), content: 'Fifth task' },
+];
 
-// Import an engine for Drag&Drop
-import Container from './engine/Container'
+const itemsPackTwo= [
+    { id: uuid(), content: 'First task' },
+    { id: uuid(), content: 'Second task' },
+    { id: uuid(), content: 'Third task' },
+]
 
-const App = () => {
-    return (
-        <div className="flex justify-center align-center mt-10">
-            {/* Create a Drag&Drop zone: */}
-            <DndProvider backend={HTML5Backend}>
-                <div className="flex">
-                    <Container />
-                </div>
+const itemsPackThree= [
+    { id: uuid(), content: 'First task' }
+]
 
-                <hr />
-
-                <div className="flex">
-                    <Container />
-                </div>
-            </DndProvider>
-        </div>
-    );
+const columnsFromBackend = {
+  [uuid()]: {
+    name: 'Requested',
+    items: itemsPackOne,
+  },
+  [uuid()]: {
+    name: 'To do',
+    items: itemsPackTwo,
+  },
+  [uuid()]: {
+    name: 'Done',
+    items: itemsPackThree,
+  }
 };
 
-export default App
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
+
+function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
+
+  return (
+    <div className="bg-blue-200 p-8 flex justify-center font-sans h-screen">
+      <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+        {Object.entries(columns).map(([columnId, column], index) => {
+          return (
+            <div key={columnId}>
+              <div className="rounded bg-gray-200 w-66 p-2 m-2" key={columnId}>
+                <div className="flex justify-between py-1">
+                  <h3 className="text-sm">{column.name}</h3>
+                  <svg
+                    className="h-4 fill-current text-grey-dark cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24">
+                    <path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" />
+                  </svg>
+                </div>
+
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="rounded bg-gray-200 w-64 p-2">
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    className="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                    {item.content}
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
+                  }}
+                </Droppable>
+                <p className="mt-3 text-gray-600">Add a card...</p>
+              </div>
+            </div>
+          );
+        })}
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default App;
